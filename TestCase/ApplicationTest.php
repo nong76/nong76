7508a30bf05bcd8f1ca4c044f3cd1f6275d4891a
nong76 <ban55zheng@163.com>
2018-04-10 11:53:28 +0800

    23jd部署pro

---
+++ TestCase/ApplicationTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed unde
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.3.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Test\TestCase;
+
+use App\Application;
+use Cake\Error\Middleware\ErrorHandlerMiddleware;
+use Cake\Http\MiddlewareQueue;
+use Cake\Routing\Middleware\AssetMiddleware;
+use Cake\Routing\Middleware\RoutingMiddleware;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * ApplicationTest class
+ */
+class ApplicationTest extends IntegrationTestCase
+{
+
+    /**
+     * testMiddleware
+     * @date    2018-04-10 10:42:22
+     */
+    public function testMiddleware()
+    {
+        $app = new Application(dirname(dirname(__DIR__)) . '/config');
+        $middleware = new MiddlewareQueue();
+
+        $middleware = $app->middleware($middleware);
+
+        $this->assertInstanceOf(ErrorHandlerMiddleware::class, $middleware->get(0));
+        $this->assertInstanceOf(AssetMiddleware::class, $middleware->get(1));
+        $this->assertInstanceOf(RoutingMiddleware::class, $middleware->get(2));
+    }
+}
